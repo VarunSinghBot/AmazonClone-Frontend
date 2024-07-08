b35a937bf1280718ed807a8f58c092237e25309e
VarunSinghBot <varunsinghbot@gmail.com>
2024-07-08 13:40:29 +0530

    cards css checked

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -69,7 +69,7 @@ const Image = styled.div`
     align-items:center;
     justify-content:center;
     flex:.6;
-    border:1px solid red;
+    /* border:1px solid red; */
     img{
         width: 100%;
         height: 100%;
